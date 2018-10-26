fde2b6fd5fb711744c83913716243a44a721ec53
Kyle VanDenHeuvel <vandenheuvelkr@gmail.com>
2018-10-25 21:28:00 -0400

    Added game board matrix (no functionality)

--- minesweeper.py
+++ minesweeper.py
@@ -1,6 +1,16 @@
+# Size of the map
+size = 10
 
-width = 10
-height = 10
+# Creates a matrix of size "size" to store the map
+game_map = []
 
-#Creates a game board matrix of size 10
-Matrix = [[0 for x in range(width)] for y in range(height)]
+for i in range(size):
+    game_map.append(["0"] * 10)
+
+def print_map(map1):
+    for row in map1:
+        print(" ".join(row))
+
+
+print(str(size))
+print_map(game_map)
