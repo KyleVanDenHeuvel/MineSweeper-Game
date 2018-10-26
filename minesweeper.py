3781f69c4ad577fb5d78ece0d0af791a04a01675
Kyle VanDenHeuvel <vandenheuvelkr@gmail.com>
2018-10-25 22:35:14 -0400

    Added second map for mines and more comments

--- minesweeper.py
+++ minesweeper.py
@@ -3,10 +3,21 @@ size = 10
 
 # Creates a matrix of size "size" to store the map
 game_map = []
+# Creates a matrix of size "size" to store where mines are located
+game_mines = []
 
+# Initializes the matrix for game_map
 for i in range(size):
-    game_map.append(["0"] * 10)
+    game_map.append(["[]"] * 10)
 
+# Initializes the matrix for game_mines
+for i in range(size):
+    game_mines.append(["[]"] * 10)
+
+'''
+Prints out the matrix which holds the game map.  Can be used to
+display both the game map and the mine map.
+'''
 def print_map(map1):
     for row in map1:
         print(" ".join(row))
